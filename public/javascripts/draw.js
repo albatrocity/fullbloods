39c5bb306e89194ba928be84ff88b9155800ed01
Ross Brown <ross@cremalab.com>
2015-07-21 07:58:14 -0500

    drawing triangles at scale and rotation

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -1,5 +1,20 @@
 var drawer;
 
+var setRotation = function (coords, xm, ym, a) {
+    var cos = Math.cos,
+        sin = Math.sin,
+
+        a = a * Math.PI / 180, // Convert to radians because that is what
+                               // JavaScript likes
+
+        // Subtract midpoints, so that midpoint is translated to origin
+        // and add it in the end again
+        xr = (coords.x - xm) * cos(a) - (coords.y - ym) * sin(a)   + xm,
+        yr = (coords.x - xm) * sin(a) + (coords.y - ym) * cos(a)   + ym;
+        coords.x = xr;
+        coords.y = yr;
+    return coords;
+}
 
 
 drawer = {
@@ -20,27 +35,50 @@ drawer = {
     var i;
     if (plots.length) {
       this.ctx.beginPath();
-      this.ctx.moveTo(plots[0].x, plots[0].y);
+      // this.ctx.moveTo(plots[0].x, plots[0].y);
       i = 1;
       while (i < plots.length) {
-        this.ctx.strokeStyle = '#785fa6';
-        this.ctx.lineTo(plots[i].x, plots[i].y);
+        this.ctx.strokeStyle = '#40208c';
+        var points = this.calcShapePos(plots, i);
+        var pi = 0;
+        while (pi < points.length) {
+          this.ctx.lineTo(points[pi].x, points[pi].y);
+          pi++;
+        }
         i++;
       }
+      // this.ctx.closePath();
       this.ctx.stroke();
     }
   },
   draw: function(e) {
     var x, y;
     x = e.clientX || e.layerX - this.canvas.offsetLeft;
-    y = (e.clientY - 38 + window.scrollY) || e.layerY - this.canvas.offsetTop;
-
+    y = (e.clientY + window.scrollY) || e.layerY - this.canvas.offsetTop;
     this.temp.push({
       x: x,
       y: y
     });
-
     this.drawOnCanvas(this.temp);
+  },
+  calcShapePos: function(plots, i) {
+    var scale = 2.5;
+    xDiff = plots[i].x - plots[i-1].x
+    xAbs = Math.abs(xDiff);
+    yAbs = Math.abs(plots[i].y - plots[i-1].y)
+
+    var length = (xAbs + yAbs)*scale;
+    var points = [];
+    var top   = {x: plots[i].x, y: plots[i].y - (length/2)}
+    var right = {x: plots[i].x + (length/2), y: plots[i].y + (length/2)}
+    var left  = {x: plots[i].x - (length/2), y: plots[i].y + (length/2)}
+
+    top   = setRotation(top, plots[i].x, plots[i].y, xDiff);
+    right = setRotation(right, plots[i].x, plots[i].y, xDiff);
+    left  = setRotation(left, plots[i].x, plots[i].y, xDiff);
+
+    points.push(top); points.push(right); points.push(left);
+    return points;
   }
 
 };

8476e8b22f5156f7adf646fcfa8c7e057b595190
Ross Brown <ross@cremalab.com>
2020-03-04 12:28:26 -0600

    Fixes import typo

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import Layout from './Layout'
+import Layout from './layout'
 
 const Page = ({ children }) => (
   <Layout pad={{ vertical: 'small' }}>{children}</Layout>

a0fe28761105c8f573fc044dd6c82c815921712c
Ross Brown <albatrocity@gmail.com>
2025-05-14 18:13:24 -0500

    fix date

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import { isBefore, isAfter } from 'date-fns'
 import type { PageLoad } from './$types'
 
 export const load: PageLoad = () => {
-  const isListeningPartyDay = isBefore(new Date(), '5/14/2025')
+  const isListeningPartyDay = isBefore(new Date(), '5/15/2025')
   const isListeningPartyActive =
     isBefore(new Date(), new Date('5/15/2025')) &&
     isAfter(new Date(), new Date('5/14/2025 19:30:00 CST'))

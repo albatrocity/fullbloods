834349304afffb548b013bb70303e39331cc62a0
Ross Brown <albatrocity@gmail.com>
2025-05-14 19:15:55 -0500

    update time

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,7 +5,7 @@ export const load: PageLoad = () => {
   const isListeningPartyDay = isBefore(new Date(), '5/15/2025')
   const isListeningPartyActive =
     isBefore(new Date(), new Date('5/15/2025')) &&
-    isAfter(new Date(), new Date('5/14/2025 19:30:00 CST'))
+    isAfter(new Date(), new Date('5/14/2025 19:15:00 CST'))
 
   return {
     isListeningPartyDay,
